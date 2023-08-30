b42b0fc080a8012ba5180d38062a291d44cb35d3
SSMYato <samuel_ssmelo@hotmail.com>
2023-08-29 21:32:04 -0300

    Alterações no script

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,55 +1,50 @@
 const convertButton = document.querySelector('.convert-button')
-const currencySelect = document.querySelector('.currency-select')
-const secondCurrencySelect = document.querySelector('.second-currencySelect')
-const secondValueImg = document.querySelector('.second-value-img')
+const inputValue = document.querySelector('.input-currency')
+const firstCurrencyName = document.querySelector('.first-currency-name')
+const firstCurrencyValue = document.querySelector('.first-currency-value')
+const secondCurrencyName = document.querySelector('.second-currency-name')
+const secondCurrencyValue = document.querySelector('.second-currency-value')
+const firstImg = document.querySelector('.first-currency-img')
+const secondImg = document.querySelector('.second-currency-img')
 
-function convertValues(){
-    const inputCurrencyValue = document.querySelector('.input-currency').value
-    const currencyValueToConvert = document.querySelector('.currency-value-to-convert') /*Primeiro valor*/
-    const currencyValueConverted = document.querySelector('.currency-value') /*Segundo valor*/
-
-    console.log(currencySelect.value)
-    console.log(secondCurrencySelect.value)
+function convertValues() {
     const dolarToday = 4.85
-    const euroToday = 5.28
     const libraToday = 6.13
-    const realToday = 1.0
+    const euroToday = 5.28
+    const firstSelect = document.querySelector('.first-select').value
+    const secondSelect = document.querySelector('.second-select').value
 
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
+    firstCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL'
-    }).format(inputCurrencyValue)
+    }).format(inputValue.value)
 
-    if(secondCurrencySelect.value == 'second-real'){
-        currencyValueConverted.innerHTML = new Intl.NumberFormat('pt-BR', {
+    if (secondSelect == 'dolar') {
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('en', {
             style: 'currency',
-            currency: 'BRL'
-        }).format(inputCurrencyValue)
+            currency: 'USD'
+        }).format(inputValue.value / dolarToday),
+        secondCurrencyName.innerHTML = 'Dólar Americano',
+        secondImg.src = './assets/dolar.png'
     }
 
-    if(secondCurrencySelect.value == 'second-dolar'){
-        currencyValueConverted.innerHTML = new Intl.NumberFormat('en', {
-        style: 'currency',
-        currency: 'USD'
-    }).format(inputCurrencyValue / dolarToday)
-    }
-
-    if(secondCurrencySelect.value == 'second-libra'){
-        currencyValueConverted.innerHTML = new Intl.NumberFormat('un-UK', {
-            style: 'currency',
-            currency: 'GBP'
-        }).format(inputCurrencyValue / libraToday)
-    }
-
-    if(secondCurrencySelect.value == 'second-euro'){
-        currencyValueConverted.innerHTML = new Intl.NumberFormat('en-EU', {
+    if (secondSelect == 'euro') {
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('en-UK', {
             style: 'currency',
             currency: 'EUR'
-        }).format(inputCurrencyValue / euroToday)
+        }).format(inputValue.value / euroToday),
+        secondCurrencyName.innerHTML = 'Euro',
+        secondImg.src = './assets/euro.png'
     }
+    if (secondSelect == 'libra'){
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('en-UK', {
+            style: 'currency',
+            currency: 'GBP'
+        }).format(inputValue.value / libraToday),
+        secondCurrencyName.innerHTML = 'Libra Esterlina',
+        secondImg.src = './assets/libra.png'
 
-    if(secondCurrencySelect == 'second-euro'){
-        secondValueImg.style.img = './assets/euro.png'
     }
 }
+
 convertButton.addEventListener('click', convertValues)
