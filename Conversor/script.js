395a08b82fc11066c977f4df6feed1e417d9cb68
SSMYato <samuel_ssmelo@hotmail.com>
2023-08-29 21:59:01 -0300

    Alterações no segundo select

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,17 +1,18 @@
-const convertButton = document.querySelector('.convert-button')
 const inputValue = document.querySelector('.input-currency')
+const secondSelect = document.querySelector('.second-select')
+const firstImg = document.querySelector('.first-currency-img')
+const convertButton = document.querySelector('.convert-button')
+const secondImg = document.querySelector('.second-currency-img')
 const firstCurrencyName = document.querySelector('.first-currency-name')
 const firstCurrencyValue = document.querySelector('.first-currency-value')
 const secondCurrencyName = document.querySelector('.second-currency-name')
 const secondCurrencyValue = document.querySelector('.second-currency-value')
-const firstImg = document.querySelector('.first-currency-img')
-const secondImg = document.querySelector('.second-currency-img')
-const secondSelect = document.querySelector('.second-select')
 
 function convertValues() {
     const dolarToday = 4.85
     const libraToday = 6.13
     const euroToday = 5.28
+    const bitcoinToday = 132.511
     const firstSelect = document.querySelector('.first-select').value
     const secondSelect = document.querySelector('.second-select').value
 
